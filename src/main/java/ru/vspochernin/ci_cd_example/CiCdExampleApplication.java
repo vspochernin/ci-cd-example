fcf231a989de789a74c5969d087bc1994356e7c9
Vladislav Pochernin <vspochernin@gmail.com>
2024-12-01 14:05:56 +0300

    initial commit for project

---
+++ src/main/java/ru/vspochernin/ci_cd_example/CiCdExampleApplication.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.ci_cd_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vspochernin/ci_cd_example/CiCdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vspochernin.ci_cd_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
